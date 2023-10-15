3e4ebf7a861020faeb86d95e39b33c0a8b5b190a
Grace Fowler <f.llfr.mgrace@gmail.com>
2023-10-15 14:40:56 -0400

    Arrays

--- Quiz Master/Assets/Scripts/QuestionSO.cs
+++ Quiz Master/Assets/Scripts/QuestionSO.cs
@@ -7,9 +7,21 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField] string strQuestion = "Enter question text";
+    [SerializeField] string[] strAnswers = new string[4];
+    [SerializeField] int intCorrectAnswerIndex;
 
     public string GetQuestion() 
     { 
         return strQuestion; 
+    }
+
+    public string GetAnswers(int index)
+    {
+        return strAnswers[index];
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return intCorrectAnswerIndex;
     }
 }
